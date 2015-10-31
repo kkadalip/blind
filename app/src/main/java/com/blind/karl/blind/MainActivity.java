a4cd910894d6dc2a8d22407b444dca954a9c81da
Karl Kadalipp <karl.kadalipp@gmail.com>
2015-10-31 10:48:34 +0200

    Calling Kõnele as an activity (from button)

---
+++ app/src/main/java/com/blind/karl/blind/MainActivity.java
@@ -0,0 +1,69 @@
+package com.blind.karl.blind;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doSomething(View view){
+        Log.d("tag", "do something start");
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // http://stackoverflow.com/questions/2209513/how-to-start-activity-in-another-application
+
+        Intent intent = new Intent();
+        intent.setComponent(
+                new ComponentName(
+                        "ee.ioc.phon.android.speak",
+                        "ee.ioc.phon.android.speak.activity.SpeechActionActivity"));
+        startActivity(intent);
+        Log.d("tag", "do something end");
+    }
+}
