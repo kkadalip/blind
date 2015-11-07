1483487bbb81dd1b536bdc94085bdc45c0ff0b0c
Karl Kadalipp <Karl Kadalipp>
2015-11-07 23:25:16 +0200

    New empty activity for apps list and shortcut creating

--- app/src/main/java/com/blind/karl/blind/AppsActivity.java
+++ app/src/main/java/com/blind/karl/blind/AppsActivity.java
@@ -1,6 +1,14 @@
 package com.blind.karl.blind;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class AppsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.apps_main);
+    }
 }
