34ded8001097b0be95f2ab7c6b59bf0ddee8ecf6
Karl Kadalipp <Karl Kadalipp>
2015-11-08 17:28:19 +0200

    Getting apps list

--- app/src/main/java/com/blind/karl/blind/AppsActivity.java
+++ app/src/main/java/com/blind/karl/blind/AppsActivity.java
@@ -1,9 +1,19 @@
 package com.blind.karl.blind;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppsActivity extends Activity {
+
+    // http://stackoverflow.com/questions/6165023/get-list-of-installed-android-applications
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +21,28 @@ public class AppsActivity extends Activity {
 
         setContentView(R.layout.apps_main);
     }
+
+    public void GetApps(View view) {
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> apps = pm.getInstalledApplications(0);
+        List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
+
+        for(ApplicationInfo app : apps) {
+            //checks for flags; if flagged, check if updated system app
+            if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 1) {
+                installedApps.add(app);
+                //it's a system app, not interested
+            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
+                //Discard this one
+                //in this case, it should be a user-installed app
+            } else {
+                installedApps.add(app);
+            }
+        }
+        Log.d("log", installedApps.toString());
+    }
+
+    // real name and icon
+    //String label = (String)pm.getApplicationLabel(app);
+    //Drawable icon = pm.getApplicationIcon(app);
 }
