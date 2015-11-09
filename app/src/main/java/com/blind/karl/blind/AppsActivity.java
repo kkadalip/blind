0a5e02f975aca224ea87c41ca9bb8f886dd30e4d
Karl Kadalipp <Karl Kadalipp>
2015-11-09 12:54:03 +0200

    Select list item, press shortcut button, open app

--- app/src/main/java/com/blind/karl/blind/AppsActivity.java
+++ app/src/main/java/com/blind/karl/blind/AppsActivity.java
@@ -1,12 +1,16 @@
 package com.blind.karl.blind;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -16,20 +20,24 @@ import java.util.List;
 
 public class AppsActivity extends Activity {
 
-
     // http://stackoverflow.com/questions/6165023/get-list-of-installed-android-applications
+    Button btnShortcut;
+    String appPackageName; // shortcut
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.apps_main);
+
+        btnShortcut = (Button) this.findViewById(R.id.btnShortcut);
     }
 
     public void GetApps(View view) {
         PackageManager pm = getPackageManager();
         List<ApplicationInfo> apps = pm.getInstalledApplications(0);
-        List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
+        final List<ApplicationInfo> installedApps = new ArrayList<>();
+        final List<String> installedAppsPackages = new ArrayList<>();
 
         List<String> installedAppsNames = new ArrayList<String>();
 
@@ -44,6 +52,19 @@ public class AppsActivity extends Activity {
                 android.R.layout.simple_list_item_1, //simple_expandable_list_item_1
                 list);
         listViewApps.setAdapter(arrayAdapter);
+        // setOnItemSelectedListener
+        //listViewApps.setClickable(true);
+        listViewApps.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = listViewApps.getItemAtPosition(position);
+                String oName = o.toString();
+                btnShortcut.setText(oName);
+
+                appPackageName = installedApps.get(position).processName.toString();
+                Log.d("log", "app appPackage is " + appPackageName);
+            }
+        });
 
         for(ApplicationInfo app : apps) {
             if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 1) {  // Updated system app, add
@@ -62,4 +83,23 @@ public class AppsActivity extends Activity {
         Log.d("log", installedApps.toString());
         Log.d("log", installedAppsNames.toString());
     }
+
+    public void Shortcut(View v){
+        //startNewActivity(this, appPackageName);
+        PackageManager pm = getPackageManager();
+        Intent intent = pm.getLaunchIntentForPackage(appPackageName);
+        this.startActivity(intent);
+    }
+
+    public void startNewActivity(Context context, String packageName) {
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent == null) {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
 }
