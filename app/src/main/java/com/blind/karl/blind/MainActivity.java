3550ac9ba32d4a294e1807a278f732db3cb2bb1d
Karl Kadalipp <Karl Kadalipp>
2015-11-09 21:20:54 +0200

    Making layouts make more sense

--- app/src/main/java/com/blind/karl/blind/MainActivity.java
+++ app/src/main/java/com/blind/karl/blind/MainActivity.java
@@ -65,14 +65,14 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         String grammar_supporting_server = "ee.ioc.phon.android.speak.service.HttpRecognitionService";
         String continuous_full_duplex_server = "ee.ioc.phon.android.speak.service.WebSocketRecognitionService";
