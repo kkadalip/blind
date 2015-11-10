54055883ad1431e9026f9f2442ad8dbc16a14668
Karl Kadalipp <Karl Kadalipp>
2015-11-10 16:26:07 +0200

    Settings activity, apps extra opens on 1st button

--- app/src/main/java/com/blind/karl/blind/AppsActivity.java
+++ app/src/main/java/com/blind/karl/blind/AppsActivity.java
@@ -3,6 +3,7 @@ package com.blind.karl.blind;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class AppsActivity extends Activity {
@@ -14,7 +15,7 @@ public class AppsActivity extends Activity {
         setContentView(R.layout.apps_main);
     }
 
-    public void gridButtonClick(){
+    public void gridButtonClick(View v){
         Intent intent = new Intent(this, AppsExtraActivity.class);
         this.startActivity(intent);
     }
