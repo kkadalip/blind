41338281d069a40949d2f8e91a7d2627d43a2aa2
Karl Kadalipp <Karl Kadalipp>
2015-11-15 06:37:18 +0200

    Broken state! Trying to swap out TabHost to pager adapter & fragments, a bit more working, not really though

--- app/src/main/java/com/blind/karl/blind/AppsActivity.java
+++ app/src/main/java/com/blind/karl/blind/AppsActivity.java
@@ -1,5 +1,6 @@
 package com.blind.karl.blind;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.os.Vibrator;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
@@ -82,6 +84,38 @@ public class AppsActivity extends FragmentActivity {
         myViewPager = (ViewPager) findViewById(R.id.myViewPager);
         myViewPager.setAdapter(myPagerAdapter);
         myViewPager.setCurrentItem(3);
+
+        final ActionBar actionBar = getActionBar();
+
+        // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create a tab listener that is called when the user changes tabs.
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+                // show the given tab
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
+
+        // Add 3 tabs, specifying the tab's text and TabListener
+        for (int i = 0; i < 3; i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText("Tab " + (i + 1))
+                            .setTabListener(tabListener));
+        }
+
 
         /*// Create the TabHost that will contain the Tabs
         tabHost = (TabHost) findViewById(android.R.id.tabhost);
--- app/src/main/java/com/blind/karl/blind/AppsFragment.java
+++ app/src/main/java/com/blind/karl/blind/AppsFragment.java
@@ -14,10 +14,8 @@ public class AppsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        // The last two arguments ensure LayoutParams are inflated
-        // properly.
-        View rootView = inflater.inflate(
-                R.layout.apps_main, container, false);
+        // The last two arguments ensure LayoutParams are inflated properly.
+        View rootView = inflater.inflate(R.layout.apps_buttons, container, false);
         Bundle args = getArguments();
         Log.d("log", "args are " + Integer.toString(args.getInt(ARG_OBJECT)));
 //        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
